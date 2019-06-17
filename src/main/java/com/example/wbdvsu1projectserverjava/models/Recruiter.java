567a1b257ab936916fcad1da7eab5438a693e533
Sujil Shah <sujil.shah@gmail.com>
2019-06-17 17:40:05 -0400

    Some Reformatting done

--- src/main/java/com/example/wbdvsu1projectserverjava/models/Recruiter.java
+++ src/main/java/com/example/wbdvsu1projectserverjava/models/Recruiter.java
@@ -5,18 +5,18 @@ import javax.persistence.Entity;
 @Entity
 public class Recruiter extends User {
 
-	private String company;
+  private String company;
 
-	public String getCompany() {
-		return company;
-	}
+  public String getCompany() {
+    return company;
+  }
 
-	public void setCompany(String company) {
-		this.company = company;
-	}	
+  public void setCompany(String company) {
+    this.company = company;
+  }
 
-	private Recruiter() {
-		
-	}
-	
+  private Recruiter() {
+
+  }
+
 }
--- src/main/java/com/example/wbdvsu1projectserverjava/models/Student.java
+++ src/main/java/com/example/wbdvsu1projectserverjava/models/Student.java
@@ -3,36 +3,44 @@ package com.example.wbdvsu1projectserverjava.models;
 import javax.persistence.Entity;
 
 @Entity
-public class Student extends User{
-	
-	private String university;
-	private float gpa;
-	private String linkedIn;
-	private String degree;
-	public String getUniversity() {
-		return university;
-	}
-	public void setUniversity(String university) {
-		this.university = university;
-	}
-	public float getGpa() {
-		return gpa;
-	}
-	public void setGpa(float gpa) {
-		this.gpa = gpa;
-	}
-	public String getLinkedIn() {
-		return linkedIn;
-	}
-	public void setLinkedIn(String linkedIn) {
-		this.linkedIn = linkedIn;
-	}
-	public String getDegree() {
-		return degree;
-	}
-	public void setDegree(String degree) {
-		this.degree = degree;
-	}
+public class Student extends User {
 
-	
+  private String university;
+  private float gpa;
+  private String linkedIn;
+  private String degree;
+
+  public String getUniversity() {
+    return university;
+  }
+
+  public void setUniversity(String university) {
+    this.university = university;
+  }
+
+  public float getGpa() {
+    return gpa;
+  }
+
+  public void setGpa(float gpa) {
+    this.gpa = gpa;
+  }
+
+  public String getLinkedIn() {
+    return linkedIn;
+  }
+
+  public void setLinkedIn(String linkedIn) {
+    this.linkedIn = linkedIn;
+  }
+
+  public String getDegree() {
+    return degree;
+  }
+
+  public void setDegree(String degree) {
+    this.degree = degree;
+  }
+
+
 }
