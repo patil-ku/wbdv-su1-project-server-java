ebcfe0a0bb9d190521a068f08b05277315a97b19
Sujil Shah <sujil.shah@gmail.com>
2019-06-19 17:06:19 -0400

    Rahul k liya bug fix

--- src/main/java/com/example/wbdvsu1projectserverjava/repositories/FollowUsersRepository.java
+++ src/main/java/com/example/wbdvsu1projectserverjava/repositories/FollowUsersRepository.java
@@ -5,12 +5,13 @@ import com.example.wbdvsu1projectserverjava.models.FollowUsersPK;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface FollowUsersRepository extends CrudRepository<FollowUsers, FollowUsersPK> {
 
   @Query("SELECT a from FollowUsers a where a.followUsersPK.userId = :userId")
-  Iterable<FollowUsers> getAllFollowingUsers(int userId);
+  Iterable<FollowUsers> getAllFollowingUsers(@Param("userId") int userId);
 
   @Query("SELECT a from FollowUsers a where a.followUsersPK.followingUserId = :followingUserId")
-  Iterable<FollowUsers> getAllMyFollowers(int followingUserId);
+  Iterable<FollowUsers> getAllMyFollowers(@Param("followingUserId") int followingUserId);
 }
--- src/main/java/com/example/wbdvsu1projectserverjava/repositories/UserRepository.java
+++ src/main/java/com/example/wbdvsu1projectserverjava/repositories/UserRepository.java
@@ -18,5 +18,5 @@ public interface UserRepository extends CrudRepository<User, Integer> {
   //SELECT * FROM `webdev-project`.user a
   // INNER JOIN `webdev-project`.student_skills b on a.id=b.student_id where b.skills like '%JAVA%';
   @Query("SELECT user FROM User user where first_name like %:skill% or username like %:skill%")
-  public Iterable<User> getAllUsersForSkill(String skill);
+  public Iterable<User> getAllUsersForSkill(@Param("skill") String skill);
 }
