9a95011985bd2726a4118b1a8c7ca655870c9158
Sujil Shah <sujil.shah@gmail.com>
2019-06-21 19:17:55 -0400

    bug fix for api to get recent job added by a user.

--- src/main/java/com/example/wbdvsu1projectserverjava/repositories/UserJobLinkRepository.java
+++ src/main/java/com/example/wbdvsu1projectserverjava/repositories/UserJobLinkRepository.java
@@ -18,6 +18,6 @@ public interface UserJobLinkRepository extends CrudRepository<SavedUserJobs, Int
   @Query("SELECT a from SavedUserJobs a where a.user_id = :userId and a.job_id=:jobId")
   SavedUserJobs getLinkedJob(@Param("userId") int userId, @Param("jobId") String jobId);
 
-  @Query(value = "SELECT a from SavedUserJobs a where a.user_id=:userId ORDER BY id desc limit 3", nativeQuery = true)
+  @Query(value = "SELECT * from saved_user_jobs a where a.user_id=:userId ORDER BY id desc limit 3", nativeQuery = true)
   Iterable<SavedUserJobs> getRecentlySavedJobs(String userId);
 }
